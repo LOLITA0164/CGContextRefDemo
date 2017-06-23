fa0fb3ea80f4336cf74c021345c954e07cda2e08
LOLITA <476512340@qq.com>
2017-06-23 13:31:12 +0800

    Initial Commit

---
+++ CGContextRef/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGContextRef
+//
+//  Created by LOLITA on 17/6/23.
+//  Copyright © 2017年 LOLITA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
